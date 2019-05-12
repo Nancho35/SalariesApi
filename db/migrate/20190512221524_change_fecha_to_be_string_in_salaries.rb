592b7380fbb40c166258ea169018ec616fdb1446
Hernan <hernandario35@gmail.com>
2019-05-12 17:41:28 -0500

    radiochamin

--- db/migrate/20190512221524_change_fecha_to_be_string_in_salaries.rb
+++
@@ -1,5 +0,0 @@
-class ChangeFechaToBeStringInSalaries < ActiveRecord::Migration[5.2]
-  def down
-    change_column :salaries, :fecha, :string
-  end
-end
--- db/migrate/20190512223423_create_salaries.rb
+++ db/migrate/20190512223423_create_salaries.rb
@@ -1,12 +1,11 @@
 class CreateSalaries < ActiveRecord::Migration[5.2]
   def change
     create_table :salaries do |t|
-      t.date :fecha
+      t.string :fecha
       t.float :valor
       t.text :descripcion
 
       t.timestamps
     end
-    
   end
 end
